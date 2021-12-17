93cb1daaed5e15244fe3ca21e821c4e502c61342
Sahil Gupte <sahilsachingupte@gmail.com>
2021-12-17 16:16:08 +0530

    Refactor tab spaces
    Signed-off-by: Sahil Gupte <sahilsachingupte@gmail.com>

--- src/__tests__/order.test.ts
+++ src/__tests__/order.test.ts
@@ -3,169 +3,169 @@ import { database } from "src/initializer";
 import { registerOrderRoute } from "../routes/orders";
 
 describe("/order/get", () => {
-    let server: FastifyInstance;
-    let userID: string
-    let orderID: string
+  let server: FastifyInstance;
+  let userID: string
+  let orderID: string
 
-    beforeEach(async () => {
-        userID = database.addUser({
-            name: 'Test User',
-            email: 'hjkhjk@hjkh.com',
-            phno: '+919819230849'
-        })
+  beforeEach(async () => {
+    userID = database.addUser({
+      name: 'Test User',
+      email: 'hjkhjk@hjkh.com',
+      phno: '+919819230849'
+    })
 
-        orderID = database.addOrder({
-            user: userID,
-            items: ['1', '2', '3', '4'],
-            quantity: 30
-        })
+    orderID = database.addOrder({
+      user: userID,
+      items: ['1', '2', '3', '4'],
+      quantity: 30
+    })
 
-        server = fastify({});
-        registerOrderRoute(server)
-        await server.ready();
+    server = fastify({});
+    registerOrderRoute(server)
+    await server.ready();
 
-        jest.clearAllMocks();
+    jest.clearAllMocks();
+  });
+
+  it("GET returns 200", async () => {
+    const response = await server.inject({
+      method: "GET", url: "/order/get?user_id=" + userID
     });
+    expect(response.statusCode).toEqual(200);
 
-    it("GET returns 200", async () => {
-        const response = await server.inject({
-            method: "GET", url: "/order/get?user_id=" + userID
-        });
-        expect(response.statusCode).toEqual(200);
+    const payload: ParsedOrder[] = JSON.parse(response.body)
+    expect(payload[0].id).toEqual(orderID)
+  });
 
-        const payload: ParsedOrder[] = JSON.parse(response.body)
-        expect(payload[0].id).toEqual(orderID)
-    });
-
-    afterEach(async () => {
-        database.deleteUser(userID)
-        database.deleteOrder(orderID)
-    }, 1000)
+  afterEach(async () => {
+    database.deleteUser(userID)
+    database.deleteOrder(orderID)
+  }, 1000)
 });
 
 describe("/order/add", () => {
-    let server: FastifyInstance;
-    let userID: string
+  let server: FastifyInstance;
+  let userID: string
 
-    beforeEach(async () => {
-        userID = database.addUser({
-            name: 'Test User',
-            email: 'hjkhjk@hjkh.com',
-            phno: '+919819230849'
-        })
-
-        server = fastify({});
-        registerOrderRoute(server)
-        await server.ready();
-
-        jest.clearAllMocks();
-    });
-
-    it("GET returns 200", async () => {
-        const response = await server.inject({
-            method: "POST", url: "/order/add", payload: {
-                items: ["0", "1", "2", "3"],
-                quantity: 50,
-                user: userID
-            }
-        });
-        expect(response.statusCode).toEqual(200);
-
-        const orderID = (JSON.parse(response.body) as { order_id: string }).order_id
-        expect(database.getOrder(userID)[0].id).toEqual(orderID)
-    });
-
-    afterEach(async () => {
-        database.deleteUser(userID)
+  beforeEach(async () => {
+    userID = database.addUser({
+      name: 'Test User',
+      email: 'hjkhjk@hjkh.com',
+      phno: '+919819230849'
     })
+
+    server = fastify({});
+    registerOrderRoute(server)
+    await server.ready();
+
+    jest.clearAllMocks();
+  });
+
+  it("GET returns 200", async () => {
+    const response = await server.inject({
+      method: "POST", url: "/order/add", payload: {
+        items: ["0", "1", "2", "3"],
+        quantity: 50,
+        user: userID
+      }
+    });
+    expect(response.statusCode).toEqual(200);
+
+    const orderID = (JSON.parse(response.body) as { order_id: string }).order_id
+    expect(database.getOrder(userID)[0].id).toEqual(orderID)
+  });
+
+  afterEach(async () => {
+    database.deleteUser(userID)
+  })
 });
 
 describe("/order/delete", () => {
-    let server: FastifyInstance;
-    let userID: string
-    let orderID: string
+  let server: FastifyInstance;
+  let userID: string
+  let orderID: string
 
-    beforeEach(async () => {
-        userID = database.addUser({
-            name: 'Test User',
-            email: 'hjkhjk@hjkh.com',
-            phno: '+919819230849'
-        })
-
-        orderID = database.addOrder({
-            user: userID,
-            items: ['1', '2', '3', '4'],
-            quantity: 30
-        })
-
-        server = fastify({});
-        registerOrderRoute(server)
-        await server.ready();
-
-        jest.clearAllMocks();
-    });
-
-    it("GET returns 200", async () => {
-        const response = await server.inject({
-            method: "DELETE", url: "/order/delete", payload: {
-                order_id: orderID
-            }
-        });
-        expect(response.statusCode).toEqual(200);
-        expect(database.getOrder(userID).length).toEqual(0)
-    });
-
-    afterEach(async () => {
-        database.deleteUser(userID)
+  beforeEach(async () => {
+    userID = database.addUser({
+      name: 'Test User',
+      email: 'hjkhjk@hjkh.com',
+      phno: '+919819230849'
     })
+
+    orderID = database.addOrder({
+      user: userID,
+      items: ['1', '2', '3', '4'],
+      quantity: 30
+    })
+
+    server = fastify({});
+    registerOrderRoute(server)
+    await server.ready();
+
+    jest.clearAllMocks();
+  });
+
+  it("GET returns 200", async () => {
+    const response = await server.inject({
+      method: "DELETE", url: "/order/delete", payload: {
+        order_id: orderID
+      }
+    });
+    expect(response.statusCode).toEqual(200);
+    expect(database.getOrder(userID).length).toEqual(0)
+  });
+
+  afterEach(async () => {
+    database.deleteUser(userID)
+  })
 });
 
 describe("/order/update", () => {
-    let server: FastifyInstance;
-    let userID: string
-    let orderID: string
+  let server: FastifyInstance;
+  let userID: string
+  let orderID: string
 
-    beforeEach(async () => {
-        userID = database.addUser({
-            name: 'Test User',
-            email: 'hjkhjk@hjkh.com',
-            phno: '+919819230849'
-        })
-
-        orderID = database.addOrder({
-            user: userID,
-            items: ['1', '2', '3', '4'],
-            quantity: 30
-        })
-
-        server = fastify({});
-        registerOrderRoute(server)
-        await server.ready();
-
-        jest.clearAllMocks();
-    });
-
-    it("GET returns 200", async () => {
-        const response = await server.inject({
-            method: "PUT", url: "/order/update", payload: {
-                order: {
-                    user: userID,
-                    id: orderID,
-                    items: ["5", "6"],
-                    quantity: 60
-                }
-            }
-        });
-
-        expect(response.statusCode).toEqual(200);
-        const updated = database.getOrder(userID)[0]
-        expect(updated.quantity).toEqual(60)
-        expect(updated.items.length).toEqual(2)
-        expect(updated.user.id).toEqual(userID)
-        expect(updated.id).toEqual(orderID)
-    });
-
-    afterEach(async () => {
-        database.deleteUser(userID)
+  beforeEach(async () => {
+    userID = database.addUser({
+      name: 'Test User',
+      email: 'hjkhjk@hjkh.com',
+      phno: '+919819230849'
     })
+
+    orderID = database.addOrder({
+      user: userID,
+      items: ['1', '2', '3', '4'],
+      quantity: 30
+    })
+
+    server = fastify({});
+    registerOrderRoute(server)
+    await server.ready();
+
+    jest.clearAllMocks();
+  });
+
+  it("GET returns 200", async () => {
+    const response = await server.inject({
+      method: "PUT", url: "/order/update", payload: {
+        order: {
+          user: userID,
+          id: orderID,
+          items: ["5", "6"],
+          quantity: 60
+        }
+      }
+    });
+
+    expect(response.statusCode).toEqual(200);
+    const updated = database.getOrder(userID)[0]
+    expect(updated.quantity).toEqual(60)
+    expect(updated.items.length).toEqual(2)
+    expect(updated.user.id).toEqual(userID)
+    expect(updated.id).toEqual(orderID)
+  });
+
+  afterEach(async () => {
+    database.deleteUser(userID)
+  })
 });
--- src/__tests__/user.test.ts
+++ src/__tests__/user.test.ts
@@ -3,79 +3,79 @@ import { database } from "src/initializer";
 import { registerUserRoute } from "src/routes/users";
 
 describe("/user/add", () => {
-    let server: FastifyInstance;
-    let userID: string
+  let server: FastifyInstance;
+  let userID: string
 
-    beforeEach(async () => {
-        server = fastify({});
-        registerUserRoute(server)
-        await server.ready();
+  beforeEach(async () => {
+    server = fastify({});
+    registerUserRoute(server)
+    await server.ready();
 
-        jest.clearAllMocks();
+    jest.clearAllMocks();
+  });
+
+  it("POST returns 200", async () => {
+    const response = await server.inject({
+      method: "POST", url: "/user/add", payload: {
+        name: 'Sahil Gupte',
+        email: 'sdkjfhjk@hmail.com',
+        phno: '+919819230849'
+      }
     });
+    expect(response.statusCode).toEqual(200);
 
-    it("POST returns 200", async () => {
-        const response = await server.inject({
-            method: "POST", url: "/user/add", payload: {
-                name: 'Sahil Gupte',
-                email: 'sdkjfhjk@hmail.com',
-                phno: '+919819230849'
-            }
-        });
-        expect(response.statusCode).toEqual(200);
+    const payload: { user_id: string } = JSON.parse(response.body)
+    userID = payload.user_id
+    expect(payload.user_id).toBeTruthy()
+  });
 
-        const payload: { user_id: string } = JSON.parse(response.body)
-        userID = payload.user_id
-        expect(payload.user_id).toBeTruthy()
-    });
-
-    afterEach(async () => {
-        database.deleteUser(userID)
-    })
+  afterEach(async () => {
+    database.deleteUser(userID)
+  })
 });
 
 describe("/user/add email validation", () => {
-    let server: FastifyInstance;
+  let server: FastifyInstance;
 
-    beforeEach(async () => {
-        server = fastify({});
-        registerUserRoute(server)
-        await server.ready();
+  beforeEach(async () => {
+    server = fastify({});
+    registerUserRoute(server)
+    await server.ready();
 
-        jest.clearAllMocks();
+    jest.clearAllMocks();
+  });
+
+  it("POST returns 400", async () => {
+    const response = await server.inject({
+      method: "POST", url: "/user/add", payload: {
+        name: 'Sahil Gupte',
+        email: 'sdkjfhjk',
+        phno: '+919819230849'
+      }
     });
-
-    it("POST returns 400", async () => {
-        const response = await server.inject({
-            method: "POST", url: "/user/add", payload: {
-                name: 'Sahil Gupte',
-                email: 'sdkjfhjk',
-                phno: '+919819230849'
-            }
-        });
-        expect(response.statusCode).toEqual(400);
-    });
+    expect(response.statusCode).toEqual(400);
+  });
 });
 
 describe("/user/add phno validation", () => {
-    let server: FastifyInstance;
+  let server: FastifyInstance;
 
-    beforeEach(async () => {
-        server = fastify({});
-        registerUserRoute(server)
-        await server.ready();
+  beforeEach(async () => {
+    server = fastify({});
+    registerUserRoute(server)
+    await server.ready();
 
-        jest.clearAllMocks();
+    jest.clearAllMocks();
+  });
+
+  it("POST returns 400", async () => {
+    const response = await server.inject({
+      method: "POST", url: "/user/add", payload: {
+        name: 'Sahil Gupte',
+        email: 'sdkjfhjk@hmail.com',
+        phno: 'fghfhgf'
+      }
     });
-
-    it("POST returns 400", async () => {
-        const response = await server.inject({
-            method: "POST", url: "/user/add", payload: {
-                name: 'Sahil Gupte',
-                email: 'sdkjfhjk@hmail.com',
-                phno: 'fghfhgf'
-            }
-        });
-        expect(response.statusCode).toEqual(400);
-    });
+    expect(response.statusCode).toEqual(400);
+  });
 });
